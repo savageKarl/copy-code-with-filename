2b9098668bbff61d5c9ff72d14a961f7b183426b
savageKarl <2443005004@qq.com>
2025-12-17 09:02:31 +0800

    chore: update build

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,33 +1,35 @@
-const esbuild = require('esbuild')
+import process from 'node:process'
+import esbuild from 'esbuild'
 
 const production = process.argv.includes('--production')
 const watch = process.argv.includes('--watch')
 
 /**
- * @type {import('esbuild').BuildOptions}
+ * @type {import('esbuild').BuildOptions['plugins']}
  */
-const esbuildProblemMatcherPlugin = {
-  name: 'esbuild-problem-matcher',
-
-  setup(build) {
-    build.onStart(() => {
-      console.log('[watch] build started')
-    })
-    build.onEnd((result) => {
-      result.errors.forEach(({ text, location }) => {
-        console.error(`✘ [ERROR] ${text}`)
-        console.error(`    ${location.file}:${location.line}:${location.column}:`)
+const plugins = [
+  {
+    name: 'esbuild-problem-matcher',
+    setup(build) {
+      build.onStart(() => {
+        console.log('[watch] build started')
       })
-      console.log('[watch] build finished')
-    })
+      build.onEnd((result) => {
+        result.errors.forEach(({ text, location }) => {
+          console.error(`✘ [ERROR] ${text}`)
+          console.error(`    ${location.file}:${location.line}:${location.column}:`)
+        })
+        console.log('[watch] build finished')
+      })
+    }
   }
-}
+]
 
 async function main() {
   const ctx = await esbuild.context({
     entryPoints: ['src/extension.ts'],
     bundle: true,
-    format: 'cjs',
+    format: 'cjs', // 输出依然保持 CommonJS，因为 VS Code 插件宿主对 CJS 支持最稳健
     minify: production,
     sourcemap: !production,
     sourcesContent: false,
@@ -35,10 +37,7 @@ async function main() {
     outfile: 'dist/extension.js',
     external: ['vscode'],
     logLevel: 'silent',
-    plugins: [
-      /* add to the end of plugins array */
-      esbuildProblemMatcherPlugin
-    ]
+    plugins
   })
 
   if (watch) {
